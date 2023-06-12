4ee5355946bfff545db85fa895013bc59c6554ec
babotavo419 <one.faith419@gmail.com>
2023-06-12 12:32:59 -0400

    changes to email submit and direction

--- frontend/components/AppClass.js
+++ frontend/components/AppClass.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import axios from 'axios'
 const initialMessage = ''
 const initialEmail = ''
 const initialSteps = 0
@@ -13,12 +13,22 @@ const initialState = {
 }
 
 export default class AppClass extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = initialState;
+    this.move = this.move.bind(this);
+    this.reset = this.reset.bind(this);
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+
+  }
 
 getXY = () => {
   const x = this.state.index % 3 + 1; 
   const y = Math.floor(this.state.index / 3) + 1;
+    return [x, y];
 }
-
+  
 getXYMessage = () => {
   const [x, y] = this.getXY();
   return `Coordinates (${x}, ${y})`;
@@ -50,8 +60,13 @@ getNextIndex = (direction) => {
 move = (evt) => {
   const direction = evt.target.id;
   const newIndex = this.getNextIndex(direction);
-  this.setState({ index: newIndex, steps: this.state.steps + 1 });
+  if (newIndex === this.state.index) {
+    this.setState({ message: `You can't go ${direction}` });
+  } else {
+    this.setState({ index: newIndex, steps: this.state.steps + 1, message: initialMessage });
+  }
 }
+
 
 onChange = (evt) => {
   this.setState({ email: evt.target.value });
@@ -71,40 +86,44 @@ onSubmit = async (evt) => {
   } catch (error) {
     this.setState({ message: error.response.data.message });
   }
+
+    this.setState({ email: initialEmail });
+  }
+
+
+render() {
+  const { className } = this.props
+  return (
+    <div id="wrapper" className={className}>
+        <div className="info">
+          <h3 id="coordinates">{this.getXYMessage()}</h3>
+        <h3 id="steps">You moved {this.state.steps} time{this.state.steps === 1 ? '' : 's'}</h3>
+      </div>
+          <div id="grid">
+            {
+              [0, 1, 2, 3, 4, 5, 6, 7, 8].map(idx => (
+          <div key={idx} className={`square${idx === this.state.index ? ' active' : ''}`}> 
+              {idx === this.state.index ? 'B' : null} 
+          </div>
+          ))
+            }
+          </div>
+      <div className="info">
+      <h3 id="message">{this.state.message}</h3>
+      </div>
+      <div id="keypad">
+        <button id="left" onClick={this.move}>LEFT</button>   
+        <button id="up" onClick={this.move}>UP</button>     
+        <button id="right" onClick={this.move}>RIGHT</button>  
+        <button id="down" onClick={this.move}>DOWN</button>  
+        <button id="reset" onClick={this.reset}>reset</button>  
+      </div>
+      <form onSubmit={this.onSubmit}>
+        <input id="email" type="email" placeholder="type email" value={this.state.email} onChange={this.onChange}></input>
+        <input id="submit" type="submit"></input>
+      </form>
+    </div>
+  )
+}
 }
 
-  render() {
-    const { className } = this.props
-    return (
-      <div id="wrapper" className={className}>
-        <div className="info">
-          <h3 id="coordinates">Coordinates (2, 2)</h3>
-          <h3 id="steps">You moved 0 times</h3>
-        </div>
-        <div id="grid">
-          {
-            [0, 1, 2, 3, 4, 5, 6, 7, 8].map(idx => (
-              <div key={idx} className={`square${idx === 4 ? ' active' : ''}`}>
-                {idx === 4 ? 'B' : null}
-              </div>
-            ))
-          }
-        </div>
-        <div className="info">
-          <h3 id="message"></h3>
-        </div>
-        <div id="keypad">
-          <button id="left">LEFT</button>
-          <button id="up">UP</button>
-          <button id="right">RIGHT</button>
-          <button id="down">DOWN</button>
-          <button id="reset">reset</button>
-        </div>
-        <form>
-          <input id="email" type="email" placeholder="type email"></input>
-          <input id="submit" type="submit"></input>
-        </form>
-      </div>
-    )
-  }
-}
