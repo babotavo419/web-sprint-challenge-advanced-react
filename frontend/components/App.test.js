9886078d1416d74408b883ee108a6a03f6467e75
babotavo419 <one.faith419@gmail.com>
2023-06-12 19:50:33 -0400

    new test

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -15,28 +15,29 @@ describe('<AppClass />', () => {
   });
 
   it('shows the correct initial coordinates and steps', () => {
-    expect(component.getByTestId('coordinates')).toHaveTextContent('Coordinates (2, 2)');
-    expect(component.getByTestId('steps')).toHaveTextContent('You moved 0 times');
+    expect(component.getByText(/Coordinates \(2, 2\)/)).toBeInTheDocument();
+    expect(component.getByText(/You moved 0 times/)).toBeInTheDocument();
   });
 
   it('moves correctly to the right and updates steps and coordinates', () => {
     fireEvent.click(component.getByText('RIGHT'));
-    expect(component.getByTestId('coordinates')).toHaveTextContent('Coordinates (3, 2)');
-    expect(component.getByTestId('steps')).toHaveTextContent('You moved 1 time');
+    expect(component.getByText(/Coordinates \(3, 2\)/)).toBeInTheDocument();
+    expect(component.getByText(/You moved 1 time/)).toBeInTheDocument();
   });
 
   it('does not move when hitting the boundary', () => {
     fireEvent.click(component.getByText('UP'));
     fireEvent.click(component.getByText('UP'));
-    expect(component.getByTestId('coordinates')).toHaveTextContent('Coordinates (2, 1)');
-    expect(component.getByTestId('steps')).toHaveTextContent('You moved 1 time');
+    expect(component.getByText(/Coordinates \(2, 1\)/)).toBeInTheDocument();
+    expect(component.getByText(/You moved 1 time/)).toBeInTheDocument();
   });
 
   it('resets the game correctly', () => {
     fireEvent.click(component.getByText('RIGHT'));
     fireEvent.click(component.getByText('reset'));
-    expect(component.getByTestId('coordinates')).toHaveTextContent('Coordinates (2, 2)');
-    expect(component.getByTestId('steps')).toHaveTextContent('You moved 0 times');
+    expect(component.getByText(/Coordinates \(2, 2\)/)).toBeInTheDocument();
+    expect(component.getByText(/You moved 0 times/)).toBeInTheDocument();
   });
 });
+
 
